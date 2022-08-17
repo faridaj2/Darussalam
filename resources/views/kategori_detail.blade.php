b80aa02e415f77f25b3ecd6b299ac2ec0abd21f1
faridaj2 <ajfarid6@Gmail.com>
2022-08-17 10:34:19 -0700

    beberapa perubahan kecil

--- resources/views/kategori_detail.blade.php
+++ resources/views/kategori_detail.blade.php
@@ -187,12 +187,12 @@
                         </div>
                         <div class="flex ">
                             <input type="number" name="in" placeholder="Jumlah disimpan"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                class="input bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             <input type="number" name="out"
                                 class="hidden bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
                             <input type="number" name="total"
-                                class="hidden bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 
                         </div>
                     </div>
@@ -235,6 +235,11 @@
                         uang = 0;
                     }
                     $('.money-place').html(uang);
+                    $('.input').on('change', function() {
+                        let input = $(this).val();
+                        input = Number(input);
+                        alert(uang + input);
+                    });
 
 
                 });
