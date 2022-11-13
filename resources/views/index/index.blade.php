6cca19bc3c946a24995cc15050c802f4edbc1efe
faridaj2 <42547920+faridaj2@users.noreply.github.com>
2022-11-13 21:31:49 +0700

    Menambahkan Informasi

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -11,6 +11,21 @@
       <div><img class="w-full" src="https://picsum.photos/600/300?random=6"></div>
       <div><img class="w-full" src="https://picsum.photos/600/300?random=7"></div>
     </div>
+    <div class="bg-gradient p-10 flex justify-center">
+      <div class="text-left">
+        <h1 class="text-white font-Lato text-2xl font-bold mb-4">Informasi & Pendaftaran Peserta Didik Baru</h1>
+        
+          <a href="" class="inline-flex gap-2 text-white bg-white/20 p-3 hover:bg-white/30 transition rounded-lg">
+            <div class="text-5xl "><i class="fa-brands fa-whatsapp "></i></div>
+            <div>
+              <div class="font-Montserrat font-semibold">081237469396</div>
+              <div class="font-Montserrat font-normal">Informasi Pendaftaran</div>
+            </div>
+          </a>
+        
+        
+      </div>
+    </div>
    </x-slot>
 
    <x-slot name="script">
@@ -25,7 +40,7 @@ $(document).ready(function(){
         touchDrag:false,
         pullDrag:false,
         freeDrag:false,
-        dots:true,
+        dots:false,
         autoplay:true,
         autoplaySpeed:5000,
         autoplayTimeout:5000,
