34ae375e4685d305a637e1e1a85e172639024fa8
faridaj2 <42547920+faridaj2@users.noreply.github.com>
2022-11-15 08:42:50 +0700

    Penyelesaian Landing Page #1

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,125 +1,232 @@
 <x-landing-layout>
-  
 
-   <x-slot name="content">
-    {{-- Hero Slide --}}
-    <div class="owl-carousel">
-      <div><img class="w-full" src="https://picsum.photos/600/300?random=1"></div>
-      <div><img class="w-full" src="https://picsum.photos/600/300?random=2"></div>
-      <div><img class="w-full" src="https://picsum.photos/600/300?random=3"></div>
-      <div><img class="w-full" src="https://picsum.photos/600/300?random=4"></div>
-      <div><img class="w-full" src="https://picsum.photos/600/300?random=5"></div>
-      <div><img class="w-full" src="https://picsum.photos/600/300?random=6"></div>
-      <div><img class="w-full" src="https://picsum.photos/600/300?random=7"></div>
-    </div>
-    {{-- End Hero SLide --}}
 
-    {{-- Informasi Pendaftaran --}}
-    <div class="bg-gradient p-10 flex justify-center">
-      <div class="text-left">
-        <h1 class="text-white font-Lato text-2xl font-bold mb-4">Informasi & Pendaftaran Peserta Didik Baru</h1>
-        
-          <a href="" class="inline-flex gap-2 text-white bg-white/20 p-3 hover:bg-white/30 transition rounded-lg">
-            <div class="text-5xl "><i class="fa-brands fa-whatsapp "></i></div>
-            <div>
-              <div class="font-Montserrat font-semibold">081237469396</div>
-              <div class="font-Montserrat font-normal">Informasi Pendaftaran</div>
-            </div>
-          </a>
-        
-        
-      </div>
-    </div>
-    {{-- End Informasi Pendaftaran --}}
-
-    {{-- Pendidikan --}}
-    <div class="pattern p-8">
-      <h1 class="text-center bg-transparent font-Lato font-bold text-2xl">
-        <span class="text-[#60A547]">Program</span>
-        <span class="text-[#648459]">Pendidikan</span>
-      </h1>
-      
-      <div class="border-b-2 my-6g mx-auto md:w-1/2 border-[#60A547] border-dashed"></div>
-
-      <div class="flex justify-center w-full p-5">
-        <div class="grid grid-cols-1 md:grid-cols-12 gap-7 md:w-2/3 md:p-2">
-
-          <div class="col-span-4 bg-white shadow-lg rounded-[20px] p-2 text-[#60A547] p-10 hover:scale-105 transition">
-            <div class="flex flex-col justify-center">
-              <div class="font-Lato font-bold text-2xl basis-1/12 text-center">
-                SMK DARUSSALAM BLOKAGUNG 2
-              </div>
-              <div class="border-b-2 w-3/4 my-4 border-[#60A547] mx-auto">
-                
-              </div>
-            </div>
-            <div class="text-center font-Montserrat p-2">
-              Menjadi Sekolah Menengah Kejuruan yang berkarakter, berbudaya, berwawasan lingkungan, menghasilkan insan yang bertaqwa, profesional, mampu berwirausaha dan berdaya saing global
-            </div>
-          </div>
-
-          <div class="col-span-4 bg-gradient shadow-lg rounded-[20px] p-2 text-white p-10 hover:scale-105 transition">
-            <div class="flex flex-col justify-center">
-              <div class="font-Lato font-bold text-2xl basis-1/12 text-center">
-                SMP DARUSSALAM BLOKAGUNG 2
-              </div>
-              <div class="border-b-2 w-3/4 my-4 border-white mx-auto">
-                
-              </div>
-            </div>
-            <div class="text-center font-Montserrat p-2">
-              Menjadi Sekolah Menengah Kejuruan yang berkarakter, berbudaya, berwawasan lingkungan, menghasilkan insan yang bertaqwa, profesional, mampu berwirausaha dan berdaya saing global
-            </div>
-          </div>
-
-          <div class="col-span-4 bg-white shadow-lg rounded-[20px] p-2 text-[#60A547] p-10 hover:scale-105 transition">
-            <div class="flex flex-col justify-center">
-              <div class="font-Lato font-bold text-2xl basis-1/12 text-center">
-                Madrasah Diniyah Darul Adzkiya
-              </div>
-              <div class="border-b-2 w-3/4 my-4 border-[#60A547] mx-auto">
-                
-              </div>
-            </div>
-            <div class="text-center font-Montserrat p-2">
-              Menjadi Sekolah Menengah Kejuruan yang berkarakter, berbudaya, berwawasan lingkungan, menghasilkan insan yang bertaqwa, profesional, mampu berwirausaha dan berdaya saing global
-            </div>
-          </div>
-          
+    <x-slot name="content">
+        {{-- Hero Slide --}}
+        <div class="owl-carousel">
+            <div><img class="w-full" src="https://picsum.photos/600/300?random=1"></div>
+            <div><img class="w-full" src="https://picsum.photos/600/300?random=2"></div>
+            <div><img class="w-full" src="https://picsum.photos/600/300?random=3"></div>
+            <div><img class="w-full" src="https://picsum.photos/600/300?random=4"></div>
+            <div><img class="w-full" src="https://picsum.photos/600/300?random=5"></div>
+            <div><img class="w-full" src="https://picsum.photos/600/300?random=6"></div>
+            <div><img class="w-full" src="https://picsum.photos/600/300?random=7"></div>
         </div>
-      </div>
-    </div>
-    {{-- End Pendidikan --}}
+        {{-- End Hero SLide --}}
+
+        {{-- Informasi Pendaftaran --}}
+        <div class="bg-gradient p-10 flex justify-center">
+            <div class="text-left">
+                <h1 class="text-white font-Lato text-2xl font-bold mb-4">Informasi & Pendaftaran Peserta Didik Baru</h1>
+
+                <a href=""
+                    class="inline-flex gap-2 text-white bg-white/20 p-3 hover:bg-white/30 transition rounded-lg">
+                    <div class="text-5xl "><i class="fa-brands fa-whatsapp "></i></div>
+                    <div>
+                        <div class="font-Montserrat font-semibold">081237469396</div>
+                        <div class="font-Montserrat font-normal">Informasi Pendaftaran</div>
+                    </div>
+                </a>
+
+
+            </div>
+        </div>
+        {{-- End Informasi Pendaftaran --}}
+
+        {{-- Pendidikan --}}
+        <div class="pattern p-8">
+            <h1 class="text-center bg-transparent font-Lato font-bold text-2xl">
+                <span class="text-[#60A547]">Program</span>
+                <span class="text-[#648459]">Pendidikan</span>
+            </h1>
+
+            <div class="border-b-2 my-6 mx-auto md:w-1/2 border-[#60A547] border-dashed"></div>
+
+            <div class="flex justify-center w-full p-5">
+                <div class="grid grid-cols-1 md:grid-cols-12 gap-7 md:w-2/3 md:p-2">
+
+                    <div
+                        class="col-span-4 md:col-span-10 lg:col-span-12 xl:col-span-4 bg-white shadow-lg rounded-[20px] p-2 text-[#60A547] p-10 hover:scale-105 transition">
+                        <div class="flex flex-col justify-center">
+                            <div class="font-Lato font-bold text-2xl basis-1/12 text-center">
+                                SMK DARUSSALAM BLOKAGUNG 2
+                            </div>
+                            <div class="border-b-2 w-3/4 my-4 border-[#60A547] mx-auto">
+
+                            </div>
+                        </div>
+                        <div class="text-center font-Montserrat p-2">
+                            Menjadi Sekolah Menengah Kejuruan yang berkarakter, berbudaya, berwawasan lingkungan,
+                            menghasilkan insan yang bertaqwa, profesional, mampu berwirausaha dan berdaya saing global
+                        </div>
+                    </div>
+
+                    <div
+                        class="col-span-4 md:col-span-10 lg:col-span-12 xl:col-span-4 bg-gradient shadow-lg rounded-[20px] p-2 text-white p-10 hover:scale-105 transition">
+                        <div class="flex flex-col justify-center">
+                            <div class="font-Lato font-bold text-2xl basis-1/12 text-center">
+                                SMP DARUSSALAM BLOKAGUNG 2
+                            </div>
+                            <div class="border-b-2 w-3/4 my-4 border-white mx-auto">
+
+                            </div>
+                        </div>
+                        <div class="text-center font-Montserrat p-2">
+                            Menjadi Sekolah Menengah Kejuruan yang berkarakter, berbudaya, berwawasan lingkungan,
+                            menghasilkan insan yang bertaqwa, profesional, mampu berwirausaha dan berdaya saing global
+                        </div>
+                    </div>
+
+                    <div
+                        class="col-span-4 md:col-span-10 lg:col-span-12 xl:col-span-4 bg-white shadow-lg rounded-[20px] p-2 text-[#60A547] p-10 hover:scale-105 transition">
+                        <div class="flex flex-col justify-center">
+                            <div class="font-Lato font-bold text-2xl basis-1/12 text-center">
+                                Madrasah Diniyah Darul Adzkiya
+                            </div>
+                            <div class="border-b-2 w-3/4 my-4 border-[#60A547] mx-auto">
+
+                            </div>
+                        </div>
+                        <div class="text-center font-Montserrat p-2">
+                            Menjadi Sekolah Menengah Kejuruan yang berkarakter, berbudaya, berwawasan lingkungan,
+                            menghasilkan insan yang bertaqwa, profesional, mampu berwirausaha dan berdaya saing global
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        {{-- End Pendidikan --}}
+
+        {{-- Visi & Misi --}}
+
+        <div class="bg-gradient font-Lato text-white font-Lato font-normal py-10 px-12">
+            <div class="mx-auto md:w-1/2 text-center">
+                <div>
+                    <h1 class="font-bold text-3xl py-10">Visi</h1>
+                    <p class="font-Montserrat">Menjadi lembaga yang unggul dan kompetitif dalam penyelenggaraan
+                        pendidikan agama yang dapat
+                        melahirkan santri yang Amil Ilmiah, Ilmiah Amaliah dan ber-Akhlakul Karimah.
+                    </p>
+                </div>
+                <div class="">
+                    <h1 class="font-bold text-3xl py-10 ">Misi</h1>
+                    <p class="">
+                    <ol class="text-left list-decimal font-Montserrat">
+                        <li>Membangun santri yang ber-IMTAQ dan ber-Akhlaqul Karimah.</li>
+                        <li>Mengantarkan santri memiliki kematangan dan kedalaman ilmu agama Islam (diniyah)</li>
+                        <li>Membekali santri dengan perangkat metodologi berfikir kritis dan mampu mentransformasikan
+                            kutubat-turats dalam era kekinian.</li>
+                        <li>Melahirkan santri yang menjadi kader dan penerus perjuangan ulama di masyarakat.</li>
+                        <li>Mengamalkan dan menjadi teladan dalam kehidupan bermasyarakat.</li>
+                    </ol>
 
 
 
-   </x-slot>
 
-   <x-slot name="script">
-    <script src="{{ asset('dist/owl.carousel.min.js') }}"></script> 
-    <script>
-      
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-        number:1,
-        loop:true,
-        mouseDrag:false,
-        touchDrag:false,
-        pullDrag:false,
-        freeDrag:false,
-        dots:false,
-        autoplay:true,
-        autoplaySpeed:5000,
-        autoplayTimeout:5000,
-        slideTransition: 'ease-in-out',
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        margin:10,
-        items:1,
-        nav: true
-    });
-  
-});
-    </script> 
-   </x-slot>
-</x-app-layout>
+
+
+
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+        {{-- End Visi & Misi --}}
+
+        {{-- Gallery --}}
+        <div class="pattern p-12">
+            <h1 class="text-center bg-transparent font-Lato font-bold text-2xl">
+                <span class="text-[#60A547] mt-10">Gallery</span>
+            </h1>
+
+            <div class="border-b-2 my-6 mx-auto md:w-1/2 border-[#60A547] border-dashed"></div>
+
+            <div class="md:w-2/3  mx-auto">
+                <div class="grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 gallery">
+                    <img class="rounded-lg" src="https://picsum.photos/300/300.jpg?random=1" alt="">
+                    <img class="rounded-lg" src="https://picsum.photos/300/300.jpg?random=2" alt="">
+                    <img class="rounded-lg" src="https://picsum.photos/300/300.jpg?random=3" alt="">
+                </div>
+            </div>
+        </div>
+        {{-- End Gallery --}}
+
+        {{-- Footer --}}
+        <div class="w-full bg-gradient py-14">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 w-10/12 mx-auto border-b-[1px] pb-10">
+                <div class="w-full">
+
+
+                    <div class="text-left text-rose-50">
+                        <div class="font-Lato text-sm md:text-lg font-bold mb-2">Yayasan PP. Darussalam Blokagung 2</div>
+                        <div class="color-decor">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div class="font-Lato text-white mt-5">
+                      <span class="font-bold">Alamat :</span>Jl. Raden Supono Krajan Setail Genteng Banyuwangi, 68465
+                    </div>
+                    <div class="text-white mt-5 font-medium">
+                      <div><i class="fa-solid fa-envelope"></i><span class="ml-3">darstachannel@gmail.com</span></div>
+                      <div><i class="fa-solid fa-phone"></i><span class="ml-3">(+62) 81 946 728 375</span></div>
+                    </div>
+
+                </div>
+                
+                <div class="w-full text-white font-Lato">
+                    <h1 class="font-bold">Profile</h1>
+                    <p class="mt-5">Tentang Kami</p>
+                </div>
+                <div class="w-full">
+                    
+                </div>
+                <div class="w-full">
+                    
+                </div>
+            </div>
+            <div class="md:flex justify-between mx-auto w-10/12 font-Lato text-white font-medium">
+              <div>© Copyright Media Darussalam Blokagung 2 - Darsta Channel</div>
+              <div class="md:flex justify-between gap-5">
+                <div>Privacy Policy</div>
+                <div>Term & Conditions</div>
+              </div>
+            </div>
+        </div>
+
+        {{-- End Footer --}}
+
+
+
+    </x-slot>
+
+    <x-slot name="script">
+        <script src="{{ asset('dist/owl.carousel.min.js') }}"></script>
+        <script>
+            $(document).ready(function() {
+                $(".owl-carousel").owlCarousel({
+                    number: 1,
+                    loop: true,
+                    mouseDrag: false,
+                    touchDrag: false,
+                    pullDrag: false,
+                    freeDrag: false,
+                    dots: false,
+                    autoplay: true,
+                    autoplaySpeed: 5000,
+                    autoplayTimeout: 5000,
+                    slideTransition: 'ease-in-out',
+                    animateOut: 'fadeOut',
+                    animateIn: 'fadeIn',
+                    margin: 10,
+                    items: 1,
+                    nav: true
+                });
+
+            });
+        </script>
+    </x-slot>
+    </x-app-layout>
