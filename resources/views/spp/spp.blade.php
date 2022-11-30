475701af84b7d228e7374cf66430581afacbe5be
faridaj2 <42547920+faridaj2@users.noreply.github.com>
2022-11-30 12:48:49 +0700

    fix spp

--- resources/views/spp/spp.blade.php
+++ resources/views/spp/spp.blade.php
@@ -5,24 +5,36 @@
         </h2>
     </x-slot>
     <div class="flex justify-between p-8">
-        <div class="text-lg">Pembayaran</div>
-        <a href="{{ route('spp.create') }}" class="btn btn-info"><i class="fa-light fa-money-check-dollar-pen"></i> Buat Pembayaran</a>
+        <div class="text-lg hidden sm:block">Pembayaran</div>
+        <a href="{{ route('spp.create') }}" class="btn btn-sm btn-info"><i class="fa-light fa-money-check-dollar-pen"></i> Buat
+            Pembayaran</a>
     </div>
 
-        {{-- List Nama Pembayaran Spp --}}
-        <div class="flex flex-col gap-3 w-full px-2">
-            @foreach ($nama_spp as $spp)
-                <div class="card w-full @if($spp->status == 'active') bg-stone-100 @else bg-gray-300 @endif  shadow-xl">
-                    <div class="card-body ">
-                        <h2 class="card-title">{{ $spp->nama_pembayaran }}</h2>
-                        <p>Dari Bulan {{ $spp->bulan_awal }} sampai {{ $spp->bulan_akhir }}</p>
-
+    {{-- List Nama Pembayaran Spp --}}
+    <div class="w-full px-2 grid md:grid-cols-2 lg:grid-cols-3 gap-2">
+        @foreach ($nama_spp as $spp)
+            <div class="w-full @if ($spp->status == 'active') bg-white @else bg-gray-100 @endif  shadow-xl rounded-lg">
+                <div class="flex justify-between p-5">
+                    <div>
+                        <h2 class="card-title font-Lato">{{ $spp->nama_pembayaran }}</h2>
+                        <p class="text-xs">Dari Bulan {{ $spp->bulan_awal }} sampai {{ $spp->bulan_akhir }}</p>
                     </div>
+                    <div class="">
+                        <div class="dropdown dropdown-top dropdown-end">
+                            <label tabindex="0" class="btn btn-sm btn-primary rounded-full text-white m-1">Detail</label>
+                            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-white rounded-box w-52">
+                              <li><a href="/dashboard/spp-kos/detail-spp">Detail</a></li>
+                              <li><a>Item 2</a></li>
+                            </ul>
+                          </div>
+                    </div>
+                    
                 </div>
-            @endforeach
-        </div>
+            </div>
+        @endforeach
+    </div>
 
-        <x-slot name="script">
+    <x-slot name="script">
 
-        </x-slot>
+    </x-slot>
 </x-app-layout>
